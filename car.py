545c9e242b55d89e5242a2b250edde5f76689328
celine19981 <celinedjeddiusthb@gmail.com>
2022-02-06 21:37:03 +0100

    maj

--- car.py
+++ car.py
@@ -1,3 +1,5 @@
+import math
+from operator import mod
 class Voiture:
     def __init__(self):
         self.vitesse=0
@@ -9,8 +11,12 @@ class Voiture:
     def decellerate(self,v):
         self.vitesse-=v
     def turnRight(self,direction):
-        self.direction+=direction
+        self.direction= (self.direction-direction) % math.pi
 
     def turnLeft(self,direction):
-        self.direction+=direction
-
+        self.direction+=(self.direction+direction) % math.pi
+    def move(self):
+        self.position[0]+=math.cos(self.direction)*self.vitesse
+        self.position[1]+=math.sin(self.direction)*self.vitesse
+    
+ 
